a351e7d9e671c569dd173fb2a726dcb697451dd7
root <root@Becks-PC.localdomain>
2026-06-12 00:26:46 +0200

    second draft

--- setup_project.sh
+++ setup_project.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # Student attendacne tracker project Factory.
-
+read -p "User input:" input
+mkdir attendance_tracker_{$input} && cd attendance_tracker_{$input}
+touch attendance_checker.py
+mkdir Helpers
